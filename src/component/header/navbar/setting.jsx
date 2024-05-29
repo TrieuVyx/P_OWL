5a1f34df249a0c0eb469b97fba5c4d24aa21d5f6
TrieuVyx <trieuvyxka1@gmail.com>
2024-05-29 07:10:55 +0700

    add search course

--- src/component/header/navbar/setting.jsx
+++ src/component/header/navbar/setting.jsx
@@ -6,29 +6,36 @@ import {
 import { centerStyle, betweenStyle, margin, gap } from "../../../shortPath/styleComponent"
 import GetUser from '../../body/Client/user/event/getUser';
 import SearchCourse from './event/search.course';
-import React, {useEffect,useState} from 'react'
+import React, {useEffect,useState, useReducer} from 'react'
+import getListCourse from '../../body/Client/course/event/getListCourse';
+
 const { Search } = Input;
 
 export default function Setting() {
     const [courseName, setCourseName] = useState('');
     const HandleSearch = ()=>{
         localStorage.setItem("SearchReq", courseName)
+        
         SearchCourse()
     }
     useEffect(()=>{
-        
+        SearchCourse()
     },[])
+
     return (
         <div >
             <Space size={24} style={betweenStyle} >
                 <Search
+       
                     style={centerStyle}
                     placeholder="input search text"
                     allowClear
+                    
                     enterButton="Search"
                     size="large"
                     onSearch={HandleSearch}
                     onChange={(e) => setCourseName(e.target.value)}
+
                 />
                 <div>
                     <Badge count={0} >
